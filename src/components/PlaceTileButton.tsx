7005aebf425b004bfe4ee888ef877cf8beec966b
jpacitto <josephpacitto@gmail.com>
2022-06-04 18:13:49 -0400

    stuff

---
+++ src/components/PlaceTileButton.tsx
@@ -0,0 +1,13 @@
+import './PlaceTileButton.css';
+
+const PlaceTileButton = () => {
+    return (
+        <div className="place-tile-button-wrapper">
+            <button>
+                Place a tile
+            </button>
+        </div>
+    );
+};
+
+export default PlaceTileButton;
---
+++ src/components/PositionIndicator.tsx
@@ -0,0 +1,13 @@
+import './PositionIndicator.css';
+
+const PositionIndicator = () => {
+    return (
+        <div className="position-indicator">
+            <p>
+                (1604, 1000) 0.5x
+            </p>
+        </div>
+    );
+};
+
+export default PositionIndicator;
